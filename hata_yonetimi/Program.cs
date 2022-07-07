8d782e52856145b8c3a3e4436023ad729623dea0
altugaltay <106876176+altugaltay@users.noreply.github.com>
2022-07-07 17:43:25 +0300

    Add files via upload

---
+++ hata_yonetimi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // try
+            // {
+            // Console.WriteLine("Bir Sayi Giriniz");
+            // int sayi = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Girilen Sayi: " + sayi);
+            // }
+            // catch(Exception ex)
+            // {
+            //     Console.WriteLine("Hata" + ex.Message.ToString());
+
+            // }
+            // finally
+            // {
+            //     Console.WriteLine("İşlem Tamamlandı");
+            // }
+
+            try
+            {
+                int sayi2 = int.Parse("abc");
+                Console.WriteLine(sayi2);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş Değer Girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Format Uygun Değil");
+                Console.WriteLine(ex);
+            }
+
+
+            
+        }
+    }
+}
